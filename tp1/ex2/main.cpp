b8fadee292229cff5435ffb36822e3dd766d59db
Dounya Hamma <dounya.hamma@edu.univ-eiffel.fr>
2024-01-22 14:21:43 +0100

    tp1-ex2-q2

---
+++ tp1/ex2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Person.hpp"
+
+int main () {
+    Person person;
+    person.first_name = "Palluche";
+    person.surname = "La Faluche";
+    std::cout << "La personne s'appelle " << person.first_name << " " << person.surname << std::endl;
+    return 0;
+}
