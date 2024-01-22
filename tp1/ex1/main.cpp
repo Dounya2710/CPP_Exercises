867609e3d15d377f5d889f3849e86380bd125728
Dounya Hamma <dounya.hamma@edu.univ-eiffel.fr>
2024-01-22 14:10:09 +0100

    tp1-ex1-q5

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -5,9 +5,10 @@ int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
+    char name[20] = ""; // Si on lui passe au moins 20 caractères ou plus, 
+    // il récupère les 20 caractères et rejette le reste car la mémoire ne peut stocker plus de 20 caractères
     std::cin >> std::setw(20) >> name;
-    std::cout << "Bonjour Palluche La Falluche !" << std::endl;
+    std::cout << "Bonjour " << name << " !" << std::endl;
 
     return 0;
 }
