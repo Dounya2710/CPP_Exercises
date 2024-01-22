5a64fc3ce051bc7adfbdbe68840f67c93490fafc
Dounya Hamma <dounya.hamma@edu.univ-eiffel.fr>
2024-01-22 14:42:16 +0100

    tp1-ex2-q6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -3,15 +3,45 @@
 #include "Person.hpp"
 
 int main () {
-    Person person;
+    //Person person;
     /*person.first_name = "Palluche";
     person.surname = "La Faluche";
     std::cout << "La personne s'appelle " << person.first_name << " " << person.surname << std::endl;*/
 
-    person.setFirstName("Palluche");
+    /*person.setFirstName("Palluche");
     person.setSurname("La Faluche");
 
-    std::cout << "La personne s'appelle " << person.getFirstName() << " " << person.getSurname() << std::endl;
+    std::cout << "La personne s'appelle " << person.getFirstName() << " " << person.getSurname() << std::endl;*/
+
+    std::cout << "Nombre de personnes: ";
+    int numberOfPeople;
+    std::cin >> numberOfPeople;
+
+    Person* peopleArray = new Person[numberOfPeople];
+
+    for (int i = 0; i < numberOfPeople; ++i)
+    {
+        std::cout << "Prenom: ";
+        std::string firstName;
+        std::cin >> firstName;
+        peopleArray[i].setFirstName(firstName);
+
+        std::cout << "Nom: ";
+        std::string surName;
+        std::cin >> surName;
+        peopleArray[i].setSurname(surName);
+    }
+
+    std::cout << "Les personnes sont ";
+    for (int i = 0; i < numberOfPeople; ++i)
+    {
+        std::cout << peopleArray[i].getFirstName() << " " << peopleArray[i].getSurname();
+        if (i < numberOfPeople - 1)
+            std::cout << ", ";
+    }
+    std::cout << "." << std::endl;
+
+    delete[] peopleArray;
 
     return 0;
 }
