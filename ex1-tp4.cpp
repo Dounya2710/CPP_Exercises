dfdd5c4bc7545936443200c7eed9818d38676595
Dounya Hamma <dounya.hamma@edu.univ-eiffel.fr>
2024-02-12 17:59:51 +0100

    new files for tp4

---
+++ ex1-tp4.cpp
@@ -0,0 +1,19 @@
+#include "../ex1-tp4.hpp"
+
+#include <optional>
+
+TEST_CASE("QCM - You answered all questions")
+{
+    REQUIRE(std::optional<bool> { ANSWER_1 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_2 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_3 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_4 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_5 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_6 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_7 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_8 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_9 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_10 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_11 } != std::nullopt);
+    REQUIRE(std::optional<bool> { ANSWER_12 } != std::nullopt);
+}
