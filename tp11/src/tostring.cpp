810c1d9a5cb278bceeb149326ebe7ee5ae648224
Dounya HAMMA <dounya.hamma7@gmail.com>
2025-04-21 21:39:44 +0200

    TP11 Finished

--- tp11/src/tostring.cpp
+++ tp11/src/tostring.cpp
@@ -3,30 +3,81 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <tuple>
 #include <type_traits>
 #include <typeinfo>
 
-template <typename T>
-auto to_string(const T& data) 
-    -> typename std::enable_if<!std::is_arithmetic<T>::value, std::string>::type
-{
-    std::stringstream ss;
-    ss << "<" << typeid(data).name() << ": " << &data << ">";
-    return ss.str();
-}
+template <typename T, typename = void>
+struct is_streamable : std::false_type {};
 
 template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
-to_string(const T& value) {
-    return std::to_string(value);
+struct is_streamable<T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T>())>>
+    : std::true_type {};
+
+template <typename T, typename = void>
+struct has_to_string_method : std::false_type {};
+
+template <typename T>
+struct has_to_string_method<T, std::void_t<decltype(std::declval<T>().to_string())>>
+    : std::true_type {};
+
+
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+
+template <typename T>
+struct is_iterable<T, std::void_t<
+    decltype(std::begin(std::declval<T>())),
+    decltype(std::end(std::declval<T>()))
+>> : std::true_type {};
+
+template <typename T>
+struct is_tuple : std::false_type {};
+
+template <typename... Args>
+struct is_tuple<std::tuple<Args...>> : std::true_type {};
+
+template <typename Tuple, std::size_t... Is>
+std::string tuple_to_string(const Tuple& t, std::index_sequence<Is...>) {
+    std::ostringstream oss;
+    oss << "{";
+    ((oss << (Is == 0 ? "" : ", ") << to_string(std::get<Is>(t))), ...);
+    oss << "}";
+    return oss.str();
 }
 
-std::string to_string(const std::string& str) {
-    return str;
-}
-
-std::string to_string(const char* c) {
-    return std::string(c);
+template <typename T>
+std::string to_string(const T& value) {
+    if constexpr (std::is_same_v<T, std::string>) {
+        return value;
+    } else if constexpr (std::is_same_v<T, const char*> || std::is_same_v<T, char*>) {
+        return std::string(value);
+    } else if constexpr (std::is_arithmetic_v<T>) {
+        return std::to_string(value);
+    } else if constexpr (has_to_string_method<T>::value) {
+        return value.to_string();
+    } else if constexpr (is_iterable<T>::value && !std::is_same_v<T, std::string>) {
+        std::ostringstream oss;
+        oss << "{";
+        auto it = std::begin(value);
+        auto end = std::end(value);
+        while (it != end) {
+            oss << to_string(*it);
+            if (++it != end) oss << ", ";
+        }
+        oss << "}";
+        return oss.str();
+    } else if constexpr (is_tuple<T>::value) {
+        return tuple_to_string(value, std::make_index_sequence<std::tuple_size_v<T>>{});
+    } else if constexpr (is_streamable<T>::value) {
+        std::ostringstream oss;
+        oss << value;
+        return oss.str();
+    } else {
+        std::ostringstream oss;
+        oss << "<" << typeid(T).name() << ": " << &value << ">";
+        return oss.str();
+    }
 }
 
 class Empty
@@ -55,17 +106,17 @@ public:
 class Both : public Streamable, public Convertible
 {};
 
-template <typename T>
+/*template <typename T>
 void print_test(std::string type, T&& value) {
     std::cout << type << std::endl;
     std::cout << "** Error: value is not a std::string" << std::endl;
+}*/
+
+template <typename T>
+void print_test(const std::string& label, T&& value) {
+    std::cout << label << "\n -> " << to_string(std::forward<T>(value)) << "\n";
 }
 
-void print_test(std::string type, std::string value)
-{
-    std::cout << type << std::endl;
-    std::cout << " -> " << value << std::endl;
-}
 
 int main()
 {
